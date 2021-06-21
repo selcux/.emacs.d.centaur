140e46e87800552d456d9246c3e696b6ec7f2765
Selçuk Öztürk <selcukahmed@gmail.com>
2021-06-21 14:27:34 +0300

    Initial commit

---
+++ lisp/init-tabnine.el
@@ -0,0 +1,6 @@
+(use-package company-tabnine
+  :ensure t)
+
+(add-to-list 'company-backends #'company-tabnine)
+
+(provide 'init-tabnine)
---
+++ lisp/init-wakatime.el
@@ -0,0 +1,4 @@
+(use-package wakatime-mode
+  :ensure t)
+
+(provide 'init-wakatime)
